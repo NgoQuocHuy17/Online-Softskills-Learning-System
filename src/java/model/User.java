9d38fc070c26e3f3a20d300a8048c029f739e560
Minh <minhnnhe180738@fpt.edu.vn>
2024-09-16 18:58:20 +0700

    nothing new

--- src/java/model/User.java
+++ src/java/model/User.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
 import java.util.Date;
@@ -10,6 +6,7 @@ import java.util.Date;
  *
  * @author Minh
  */
+
 public class User {
     private int id;
     private String fullName, gender, email, passwordHash, role, avatarUrl;
@@ -101,6 +98,4 @@ public class User {
     public void setUpdatedAt(Date updatedAt) {
         this.updatedAt = updatedAt;
     }
-
-    
 }
